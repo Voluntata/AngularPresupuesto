e3f6763ea10df64ca7c3dac1b14b1a35542dbbd5
Voluntata <voluntata@gmail.com>
2021-11-25 16:23:49 +0100

    ex2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
-import { R3ConstructorFactoryMetadata } from '@angular/compiler/src/render3/r3_factory';
+
 import { Component, ElementRef, Renderer2, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Trabajo } from '../interface';
 
@@ -18,25 +18,26 @@ export class HomeComponent implements OnInit {
     { precio: 200, nombre: "Una campanya de Google Ads (200€)" }
   ];
 
-  // get trabajosFormArray() {
-  //   return this.myReactiveForm.controls['trabajos'] as FormArray;
-  // }
+
 public isChecked:boolean = false;
+public precio:number = 0;
   constructor(private router: Router, private fb: FormBuilder) {
     this.myReactiveForm = this.fb.group({
-      trabajo: this.fb.array([], Validators.required)
+      result: this.fb.array([], Validators.required)
     });
 
   }
   onCheckboxChange(e:any) {
-    const trabajo: FormArray = this.myReactiveForm.get('trabajo') as FormArray;
+
     if(e.target.checked){
-      trabajo.push(new FormControl(e.target.value));
+      let value:number = parseInt(e.target.value);
       this.isChecked = true;
+      this.precio = this.precio +value;
+
     }
     else{
-      const index = trabajo.controls.findIndex(x=> x.value === e.target.value);
-      trabajo.removeAt(index);
+      let value:number = parseInt(e.target.value);
+      this.precio = this.precio - value;
       this.isChecked = false;
     }
   }
@@ -44,18 +45,9 @@ public isChecked:boolean = false;
   ngOnInit(): void {
 
   }
-
-  getPrise() {
-    // const reducer = (acc:number, curr:number) => acc + curr;
-    // let arr = this.myReactiveForm.value;
-    // arr[0]= 500;
-    // arr.reduce(reducer);
-  return  this.myReactiveForm.value;
-  }
-
-  //public isChecked = false;
-
-
+  inputsResult(value:number) {
+    console.log(value);
+}
 }
 
 
--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-panel',
@@ -6,8 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./panel.component.css']
 })
 export class PanelComponent implements OnInit {
+@Output() inputValues = new EventEmitter();
+inputValue:number =  0;
 
-  constructor() { }
+  panelForm: FormGroup
+  constructor(private fb: FormBuilder) {
+    this.panelForm = this.fb.group({
+      'paginas': new FormControl(0, [Validators.required]),
+      'idiomas': new FormControl(0, [Validators.required]),
+    });
+  }
+
+ @ViewChild('pagInput', {static: true})
+  pagInput!: ElementRef;
+ @ViewChild('lgInput', {static: true})
+  lgInput!: ElementRef;
+
+ OnInput(){
+let pagValue:number = parseInt(this.pagInput.nativeElement.value);
+let lgValue:number = parseInt(this.lgInput.nativeElement.value);
+this.inputValue = pagValue + lgValue;
+this.inputValues.emit(this.inputValue);
+//console.log(this.inputValue)
+ }
 
   ngOnInit(): void {
   }
